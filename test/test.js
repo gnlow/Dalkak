9007994be9de5cfba082f32dfaba2ed402e81244
Gnlow <entrydark@gmail.com>
2019-09-14 10:29:30 +0900

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,3 +1,27 @@
 const dalkak = require("../dist/dalkak.js");
 
-console.log(dalkak);
+var project = new dalkak.Project();
+var start = new dalkak.Event("start");
+var log = new dalkak.Block(
+    "log", 
+    "((text))를 콘솔에 ((howmany))번 찍기", 
+    param => {
+        for(var i=0;i<param.howmany;i++){
+            console.log(param.text);
+        }
+    },
+    {
+        text: "hello",
+        howmany: 3
+    }
+);
+
+var entrybot = dalkak.Thing.fromBlock(log, start);
+
+project.addThing(entrybot)
+
+project.ready();
+start.fire();
+log.setParam("text", "ㅎㅇㅎㅇ")
+start.fire();
+console.log(project.thingGroup.children[0].blocks[0]);
