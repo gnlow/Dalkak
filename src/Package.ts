d1acaddcd6175838d03995046eccbd2a925f9fbd
Gnlow <entrydark@gmail.com>
2019-12-20 22:39:36 +0900

    Add: Package.types
    #10

--- src/Package.ts
+++ src/Package.ts
@@ -1,18 +1,22 @@
 import {Name} from "./Name";
 import {Event} from "./Event";
 import {Block} from "./Block";
+import {Type} from "./Type";
 
 export class Package{
 	name: string;
 	blocks: Array<Block>;
 	events: Array<Event>;
+	types: Array<Type>;
 	constructor(
 		name = Name.randomize(), 
 		blocks: Array<Block> = [], 
-		events: Array<Event> = []
+		events: Array<Event> = [],
+		types: Array<Type> = []
 	){
 		this.name = name;
 		this.blocks = blocks;
 		this.events = events;
+		this.types = types;
 	}
 }
