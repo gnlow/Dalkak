2c92906af8609fc96cb0f7ffc21e23a0486e3156
Gnlow <entrydark@gmail.com>
2019-12-21 23:40:53 +0900

    Change: "Array"s in "Pack" to "Dict"s

--- src/Pack.ts
+++ src/Pack.ts
@@ -2,17 +2,18 @@ import {Name} from "./Name";
 import {Event} from "./Event";
 import {Block} from "./Block";
 import {Type} from "./Type";
+import {Dict} from "./Dict";
 
 export class Pack{
 	name: string;
-	blocks: Array<Block>;
-	events: Array<Event>;
-	types: Array<Type>;
+	blocks: Dict<Block>;
+	events: Dict<Event>;
+	types: Dict<Type>;
 	constructor(
 		name = Name.randomize(), 
-		blocks: Array<Block> = [], 
-		events: Array<Event> = [],
-		types: Array<Type> = []
+		blocks: Dict<Block> = new Dict, 
+		events: Dict<Event> = new Dict,
+		types: Dict<Type> = new Dict
 	){
 		this.name = name;
 		this.blocks = blocks;
