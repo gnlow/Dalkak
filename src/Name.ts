2149e5c7da1464ffa80df52cab7de0cc001a7d82
Gnlow <entrydark@gmail.com>
2020-01-09 19:22:50 +0900

    FIx: wrong default prop in "Name"

--- src/Name.ts
+++ src/Name.ts
@@ -6,7 +6,7 @@ export class Name{
 	key: string;
 	constructor(namespace: Namespace, key = Util.randString(5)){
 		this.namespace = namespace;
-		this.key = this.namespace.names.has(key) ? key : Util.randString(5);
+		this.key = this.namespace.names.has(key) ? Util.randString(5) : key;
 		this.namespace.names.add(this.key);
 	}
 }
