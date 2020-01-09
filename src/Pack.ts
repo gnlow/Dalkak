7ca167359a2c39df6a8f5e59b3d7a35baceaea59
Gnlow <entrydark@gmail.com>
2020-01-09 14:26:09 +0900

    Change: string name to "Name" in "Pack"

--- src/Pack.ts
+++ src/Pack.ts
@@ -3,19 +3,21 @@ import {Event} from "./Event";
 import {Block} from "./Block";
 import {Type} from "./Type";
 import {Dict} from "./Dict";
+import {Util} from "./Util";
 
 export class Pack{
-	name: string;
+	name: Name;
 	blocks: Dict<Block>;
 	events: Dict<Event>;
 	types: Dict<Type>;
 	constructor(
-		name = Name.randomize(), 
+		parent = new Dict, 
+		name = Util.randString(5), 
 		blocks: Dict<Block> = new Dict, 
 		events: Dict<Event> = new Dict,
 		types: Dict<Type> = new Dict
 	){
-		this.name = name;
+		this.name = new Name(parent.namespace, name);
 		this.blocks = blocks;
 		this.events = events;
 		this.types = types;
