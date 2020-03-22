e783c5bcec1615af025580f9daa7acb199455d5b
Gnlow <entrydark@gmail.com>
2020-03-22 20:56:28 +0900

    Fix: error in "Local.getVariable()"

--- src/Local.ts
+++ src/Local.ts
@@ -19,6 +19,9 @@ export class Local {
 		return copy;
 	}
 	getVariable(name: string){
+		if(!this.variables.value[name]){
+			this.variables.value[name] = [];
+		}
 		return this.variables.value[name].find(variable => variable.scope.compare(this.scope)) 
 		|| this.variables.value[name][this.variables.value[name].push(new Variable({name}))];
 	}
